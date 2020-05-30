b30008b348f8c0bae5fc5e6581a4bb1bce61496e
Arunprasath-P <66112523+Arunprasath-P@users.noreply.github.com>
2020-05-30 13:45:07 +0530

    Add files via upload

---
+++ fractinalknapsack.c
@@ -0,0 +1,55 @@
+# include<stdio.h> 
+int fun(int n, int weight[], int profit[], int capacity) {
+   float x[20], tp = 0;
+   int i, j, u;
+   u = capacity;
+ 
+   for (i = 0; i < n; i++)
+      x[i] = 0.0;
+ 
+   for (i = 0; i < n; i++) {
+      if (weight[i] > u)
+         break;
+      else {
+         x[i] = 1.0;
+         tp = tp + profit[i];
+         u = u - weight[i];
+      }
+   }
+ 
+   if (i < n)
+      x[i] = u / weight[i];
+ 
+   tp = tp + (x[i] * profit[i]);
+ 
+   printf("\nThe result ");
+   for (i = 0; i < n; i++)
+      printf("%f\t", x[i]);
+ 
+   printf("\nMaximum profit is:- %f", tp);
+   return 0;
+ 
+}
+ 
+int main() {
+   float weight[20], profit[20], capacity;
+   int num, i, j;
+   float ratio[20], temp;
+ 
+   printf("\nEnter the no. of objects:- ");
+   scanf("%d", &num);
+ 
+   printf("\nEnter the weight and profit ");
+   for (i = 0; i < num; i++) {
+      scanf("%f %f", &weight[i], &profit[i]);
+   }
+ 
+   printf("\nEnter the capacityof knapsack:- ");
+   scanf("%f", &capacity);
+ 
+   for (i = 0; i < num; i++) {
+      ratio[i] = profit[i] / weight[i];
+   } 
+   fun(num, weight, profit, capacity);
+   return(0);
+}
